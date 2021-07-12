521578f22c2f050122ec6d60c1a9f5d1be5fe407
samsulamin <samsul.rizqiamin@gmail.com>
2021-07-12 22:46:58 +0700

    bismillah

--- config/app.php
+++ config/app.php
@@ -162,7 +162,7 @@ return [
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
         GoogleMaps\ServiceProvider\GoogleMapsServiceProvider::class,
-
+        Laravel\Passport\PassportServiceProvider::class,
         /*
          * Package Service Providers...
          */
